0189da55f43c8a44bda094c8823c221a7c397e96
Jordy <jsmall53@ymail.com>
2025-05-06 16:43:29 -0700

    adding two scripts that ive never run

---
+++ packages.sh
@@ -0,0 +1,10 @@
+
+sudo pacman -S --noconfig ripgrep gimp fzf rofi-wayland alacritty tmux
+sudo pacman -S --noconfig hyprpaper
+sudo pacman -S --noconfig bluez bluez-utils
+
+sudo pacman -S --noconfig neovim
+
+
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
