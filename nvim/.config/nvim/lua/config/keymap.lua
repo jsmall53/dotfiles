7275601c582f6e7edf6bf7c241959f3c6a7944f8
Jordy <jsmall53@ymail.com>
2025-05-07 13:04:34 -0700

    error jump keymaps

--- nvim/.config/nvim/lua/config/keymap.lua
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -16,6 +16,18 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+    local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+    severity = severity and vim.diagnostic.severity[severity] or nil
+    return function()
+        go( {severity = severity })
+    end
+end
+vim.keymap.set('n', "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+vim.keymap.set('n', "[e", diagnostic_goto(true, "ERROR"), { desc = "Prev Error" })
+
+-- yank highlight
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
